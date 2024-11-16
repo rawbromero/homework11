bd9bb522e51ac373800d69fca6af39ac3fb59132
rawbromero <129907865+rawbromero@users.noreply.github.com>
2024-11-16 13:52:35 -0600

    card detail finish

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1,11 +1,37 @@
 <script setup>
+import {ref, onMounted, onUnmounted} from 'vue'
 import { useRoute } from 'vue-router'
+import useAPI from '@/Composables/useAPI';
+import { faker } from '@faker-js/faker';
+
+const {fetchEmployee, currentEmployee} = useAPI()
+
+onMounted( async () => {
+    await fetchEmployee(route.params.id)
+})
+
+onUnmounted(() => {
+    currentEmployee.value = null
+})
+
 const route = useRoute()
 </script>
 
+
+
 <template>
+    <!-- {{ route.params.id }} -->
+    <main>
+        <div v-if="currentEmployee" class="flex flex-col items-center justify-center gap-6">
+            <h1 class="text-6xl font-bold p-5">{{ currentEmployee.firstName }} {{ currentEmployee.lastName }}</h1>
+            <h1 class="text-3xl p-5">{{ currentEmployee.title }}</h1>
+            <h1 class="text-2xl p-5">{{ currentEmployee.userName }}@southtexasccollege.edu</h1>
+            <h1 class="text-2xl p-5">{{ currentEmployee.quote }}</h1>
+            
+            <img class="p-8" :src="faker.image.urlLoremFlickr({width: 640, height: 480, category: 'dog'})"/>
+    </div>
 
-{{route.params.id}}
 
-    
+    </main>
+
 </template>
