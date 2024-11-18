4cfebdd232b9783dd33a798e5ddf546dbfd4990b
rawbromero <129907865+rawbromero@users.noreply.github.com>
2024-11-18 14:47:49 -0600

    re ran the final stage

--- src/router.js
+++ src/router.js
@@ -1,30 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
-import CardDetails from './views/CardDetails.vue'
-import LoginPage from './views/LoginPage.vue' 
-import SettingsPage from './views/SettingsPage.vue'
+import CardDetails from '@/views/CardDetails.vue'
+import LoginPage from '@/views/LoginPage.vue'
+import SettingsPage from '@/views/SettingsPage.vue'
+
+import { useAuth } from './Composables/useAuth'
+const {isAuthenticated} = useAuth()
+  
 
 const routes = [
-  { path: '/homework11-api/', name: 'Home', component: HomePage },
-  { path: '/homework11-api/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-  { path: '/homework11=api/employees/:id', name: 'CardDetails', component: CardDetails },
-  { path: '/homework11-api/login', name: 'LoginPage', component: LoginPage},
-  { path: '/homework11-api/settings', name: 'SettingsPage', component: SettingsPage, meta: { requiresAuth: true} },
-     
+  { path: '/Homework11/', name: 'Home', component: HomePage },
+  { path: '/Homework11/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/Homework11/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/Homework11/login', name: 'LoginPage', component: LoginPage },
+  { path: '/Homework11/settings', name: 'SettingsPage', component: SettingsPage,meta:{requiresAuth: true} },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
-//navigation guard
-router.beforeEach((to, _, next) => { 
-  if (to.meta.requiresAuth && !isAuthticated.value) {
-    next({ name: 'LoginPage', query: { redirect: to.fullPath } })
+
+//Navigation Guard
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated.value) {
+    next({name: 'LoginPage', query: {redirect: to.fullPath}})
   } else {
     next()
   }
-
 })
+
 export default router
